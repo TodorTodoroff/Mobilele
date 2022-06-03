cda532f3d129365277cad5ce2089eb6c2d4dcd9e
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-06-03 12:08:42 +0300

    initial commit

---
+++ src/main/java/bg/softuni/MobileleMineVersion/MobileleMineVersionApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.MobileleMineVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleMineVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileleMineVersionApplication.class, args);
+	}
+
+}
