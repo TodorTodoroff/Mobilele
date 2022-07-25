20acbaa0a72f3d782d70778d49bfbd3104466775
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-07-25 18:46:05 +0300

    added JavaMailSender

---
+++ src/main/java/bg/softuni/MobileleMineVersion/config/MailConfig.java
@@ -0,0 +1,43 @@
+package bg.softuni.MobileleMineVersion.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(
+            @Value("${mail.host}") String mailHost,
+            @Value("${mail.port}") int mailPort,
+            @Value("${mail.username}") String Username,
+            @Value("${mail.password}") String password
+    ) {
+
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost(mailHost);
+        javaMailSender.setPort(mailPort);
+        javaMailSender.setUsername(Username);
+        javaMailSender.setPassword(password);
+        javaMailSender.setJavaMailProperties(mailProperties());
+
+        return javaMailSender;
+
+    }
+
+    private Properties mailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.transport.protocol", "smtp");
+
+        return properties;
+    }
+
+
+}
